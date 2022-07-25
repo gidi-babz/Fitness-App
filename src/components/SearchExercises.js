302bc8b7d66e9f948d0d67bc2ad8c7fd48b18e1a
Gidi Babz <tunex4ind@gmail.com>
2022-07-25 17:22:34 +0100

    commits

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,7 +1,96 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { Box, TextField, Button, Stack, Typography } from '@mui/material';
+
+import { exerciseOptions, fetchData } from '../utils/FetchData';
+
+const url = 'https://exercisedb.p.rapidapi.com/exercises';
 
 const SearchExercises = () => {
-	return <div>SearchExercises</div>;
+  const [search, setSearch] = useState('');
+  const [exercises, setExercises] = useState([]);
+  const [bodyParts, setBodyParts] = useState([]);
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const bodyPartsData = await fetchData(
+        `${url}/bodyPartList`,
+        exerciseOptions
+      );
+      setBodyParts(['all', ...bodyPartsData]);
+    };
+
+    fetchExercisesData();
+    console.log(bodyParts);
+  }, []);
+
+  const handleSearch = async () => {
+    if (search) {
+      const exerciseData = await fetchData(url, exerciseOptions);
+
+      const searchedExercises = exerciseData.filter(
+        exercise =>
+          exercise.name.toLowercase().includes(search) ||
+          exercise.bodyPart.toLowercase().includes(search) ||
+          exercise.equipment.toLowercase().includes(search) ||
+          exercise.target.toLowercase().includes(search)
+      );
+
+      setSearch('');
+      setExercises(searchedExercises);
+    }
+  };
+
+  return (
+    <Stack alignItems="center" justifyContent="center" mt="37px" p="20px">
+      <Typography
+        fontWeight={700}
+        sx={{
+          fontSize: { lg: '44px', xs: '30px' },
+        }}
+        mb="50px"
+        textAlign="center"
+      >
+        Awesome Exercises You <br /> Should Know
+      </Typography>
+      <Box position="relative" mb="72px">
+        <TextField
+          sx={{
+            input: {
+              fontWeight: '600',
+              fontSize: '16px',
+              border: 'none',
+              borderRadius: '5px',
+            },
+            width: { lg: '800px', xs: '350px' },
+            backgroundColor: '#fff',
+            borderRadius: '40px',
+          }}
+          height="76px"
+          value={search}
+          onChange={e => setSearch(e.target.value.toLowerCase())}
+          placeholder="Search Exercises"
+          type="text"
+        />
+        <Button
+          className="search-btn"
+          sx={{
+            bgcolor: '#ff2625',
+            color: '#fff',
+            textTransform: 'none',
+            width: { lg: '175px', xs: '80px' },
+            fontSize: { lg: '20px', xs: '14px' },
+            height: '56px',
+            position: 'absolute',
+            right: '0',
+          }}
+          onClick={handleSearch}
+        >
+          Search
+        </Button>
+      </Box>
+    </Stack>
+  );
 };
 
 export default SearchExercises;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,13 +7,13 @@ import HeroBanner from '../components/HeroBanner';
 import SearchExercises from '../components/SearchExercises';
 
 const Home = () => {
-	return (
-		<Box>
-			<HeroBanner />
-			<SearchExercises />
-			<Exercises />
-		</Box>
-	);
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises />
+      <Exercises />
+    </Box>
+  );
 };
 
 export default Home;
